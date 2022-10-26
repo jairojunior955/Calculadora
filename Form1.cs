75be2e9767e038118af468f2d1a3c9963f9e7bd3
Jairo Santos jr <79456331+jairojunior955@users.noreply.github.com>
2022-10-26 16:48:55 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class FormMainMenu : Form
+    {
+        public FormMainMenu()
+        {
+            InitializeComponent();
+            this.Text = string.Empty;
+            this.ControlBox = false;
+        }
+
+        private void botaoFechar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void botaoMaximizar_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Normal)
+                this.WindowState = FormWindowState.Maximized;
+            else
+                this.WindowState = FormWindowState.Normal;
+        }
+
+        private void botaoMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
